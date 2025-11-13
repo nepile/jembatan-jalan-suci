02234a0ff5a3afa60b0735fdbd7a843c10148772
alfaWidiantoro <alfawidiantoro66@gmail.com>
2025-11-14 03:22:36 +0800

    feat: home page

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg bg-white shadow-sm">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="{{ asset('images/logo.png') }}" alt="Yauma Logo" height="45">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav align-items-center">
+                <li class="nav-item"><a class="nav-link text-dark fw-semibold" href="#">Home</a></li>
+                <li class="nav-item"><a class="nav-link text-dark fw-semibold" href="#">Donasi Umum</a></li>
+                <li class="nav-item"><a class="nav-link text-dark fw-semibold" href="#">Campaign</a></li>
+                <li class="nav-item"><a class="nav-link text-dark fw-semibold" href="#">Konfirmasi Donasi</a></li>
+                <li class="nav-item"><a class="nav-link text-dark fw-semibold" href="#">Akun Saya</a></li>
+                <li class="nav-item"><a class="nav-link text-dark fw-semibold" href="#">Kalkulator Zakat</a></li>
+                <li class="nav-item ms-3">
+
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
