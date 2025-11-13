0997da8049beece5d85c697ed1bc8d7891009524
alfaWidiantoro <alfawidiantoro66@gmail.com>
2025-11-14 03:18:05 +0800

    fead:home page

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.page')
+
+@section('title', 'Beranda | Donasi Yauma')
+
+@section('content')
+    <div class="text-center">
+        <h1>Selamat Datang di Donasi Yauma</h1>
+        <p>Donasi Anda Selamatkan Mereka 💚</p>
+    </div>
+@endsection
